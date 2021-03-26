187a20da0910ca9ff1b2061cd6fb95d636961959
George Atkinson <geomyles@yahoo.co.uk>
2021-03-26 09:59:47 +0000

    Added derive macro for Node trait

---
+++ cod-node-derive/src/lib.rs
@@ -0,0 +1,29 @@
+
+use quote::{quote, ToTokens};
+
+#[proc_macro_derive(Node)]
+pub fn node(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+
+    match &input.data {
+        syn::Data::Struct(data) => derive_struct(&input, data).into(),
+        _=> unimplemented!(),
+    }
+}
+
+fn derive_struct(derive_input: &syn::DeriveInput, data: &syn::DataStruct) -> proc_macro2::TokenStream {
+    let name = &derive_input.ident;
+
+    quote! {
+        impl cod::Node for #name {
+            
+            fn header(&self) -> &cod::Header {
+                &self.header
+            }
+
+            fn header_mut(&mut self) -> &mut cod::Header {
+                &mut self.header
+            }
+        }
+    }
+}
